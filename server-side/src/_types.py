a359630e94ecbbde6d684a7b3e64f2c572b2f01a
Mark M <otteromkram@gmail.com>
2021-08-01 12:53:48 -0500

    installed spacy, mxnet

--- server-side/src/_types.py
+++ server-side/src/_types.py
@@ -1,24 +1,34 @@
 
-__all__ = [
-    "Dict",
-    "List",
-    "Number",
-    "Vector",
-    "MutVector",
-    "Iterables",
-    "StrDict",
-    "Tuple",
-]
+# __all__ = [
+#     "Dict",
+#     "List",
+#     "Number",
+#     "Vector",
+#     "MutVector",
+#     "Iterables",
+#     "StrDict",
+#     "StrList",
+#     "Tuple",
+# ]
 
 
-from typing import Dict, Iterable, List, Optional, Union, Tuple
+from typing import Dict, Iterable, Iterator, List, Optional, Union, Tuple
 
 # Number for either integer or foating point value.
 Number = Union[int, float]
 
-# Numerical vector (mutable)
 Vector = List[Number]
+NumList = List[Number]
+IntList = List[int]
+FloatList = List[float]
+StrList = List[str]
+
 MutVector = Tuple[Number]
 
 Iterables = Union[Iterable, Iterable]
+
+
 StrDict = Dict[str, str]
+StrNumDict = Dict[str, Number]
+NumDict = Dict[Number, Number]
+IntDict = Dict[int, int]
